463f18ca2675bd7bbc9c4d1ecc1590b189fd3c54
Adam Ulvi <adam@delaris.com>
2012-12-16 13:46:13 -0800

    initial check-in

--- GroMon_Sketch/GroMon_Sketch.ino
+++ GroMon_Sketch/GroMon_Sketch.ino
@@ -9,29 +9,7 @@
  - #include <NewSoftSerial.h> -> #include <SoftwareSerial.h>fa
  - NewSoftSerial blueToothSerial(RxD,TxD); -> SoftwareSerial blueToothSerial(RxD,TxD);
  */
-/*
-BluetoothBee Demo Code - Flowcontrol Based Implementation
-2010,2011 Copyright (c) Seeed Technology Inc.  All right reserved.
 
-Author: Visweswara R      Modify:Joinj
-
-This demo code is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-For more details about the product please check http://www.seeedstudio.com/depot/
-
-*/
 
 #include <SoftwareSerial.h>   //Software Serial Port
 #define RxD 2  // these '2' & '3' are fixed by the hardware
@@ -39,7 +17,7 @@ For more details about the product please check http://www.seeedstudio.com/depot
 #define DEBUG_ENABLED 1
 
 SoftwareSerial blueToothSerial(RxD,TxD);
-int led = 5;
+int led = 4;
 
 void setup()
 {
@@ -64,31 +42,42 @@ void loop()
 //You can write you BT communication logic here
  
  
+  // Big event loop.
   while(1){
-  char bluetoothData = (char)blueToothSerial.read();
-  if (blueToothSerial.available()) {
-   
-    Serial.write(bluetoothData);
-  } 
-  if (bluetoothData == 'n') {
-    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  }
-  if (bluetoothData == 'f') {
-    digitalWrite(led, LOW);   // turn the LED off (LOW is the voltage level)
-  }
-   
-  if (Serial.available())
-    blueToothSerial.write(Serial.read());
-  }   
+  
+    Serial.println("Getting Data");
+  
+    char bluetoothData = (char)blueToothSerial.read();
+    
+    if (blueToothSerial.available()) {
+     
+      Serial.print("Data: ");
+      Serial.println(bluetoothData);
+      
+      Serial.write(bluetoothData);
+    }
+  
+    if (bluetoothData == 'n') {
+      digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    }
+    if (bluetoothData == 'f') {
+      digitalWrite(led, LOW);   // turn the LED off (LOW is the voltage level)
+    }
+     
+    if (Serial.available()) {
+      blueToothSerial.write(Serial.read());
+    }
+      
+  } // End while(1)
  
 }
 
 void setupBlueToothConnection()
 {
     blueToothSerial.begin(38400); //Set BluetoothBee BaudRate to default baud rate 38400
-    delay(1000);
+    delay(2000);
     sendBlueToothCommand("\r\n+STWMOD=0\r\n");    // 0 = slave
-    sendBlueToothCommand("\r\n+STNA=GroMon\r\n"); 
+    sendBlueToothCommand("\r\n+STNA=GroMonProbe\r\n"); 
     sendBlueToothCommand("\r\n+STAUTO=0\r\n");    // 0 = Auto-connect forbidden
     sendBlueToothCommand("\r\n+STOAUT=1\r\n");    // 1 = Permit Paired device to connect me
     sendBlueToothCommand("\r\n+STPIN=0000\r\n");
@@ -142,3 +131,4 @@ void sendBlueToothCommand(char command[])
 
 
 //********* end program *********
+
