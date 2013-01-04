e8424c18ac2be3e03c27e440b7c88b47b768474c
Adam Ulvi <adam@delaris.com>
2013-01-03 17:39:38 -0800

    Updated the comments in the probe sim to reflect the current node startup params.

--- Web/dummy-probe.js
+++ Web/dummy-probe.js
@@ -6,11 +6,11 @@
  * 1. Create two devices, you'll see the paths in the output:
  *    socat -d -d pty,raw,echo=0 pty,raw,echo=0
  *
- * 2. Start this script using one of the devices above:
+ * 2. Start this script using the first device from the command above:
  *    node dummy-probe.js --port=/dev/pts/5
  *
  * 3. Start the express app using the other device:
- *    node app.js --port=/dev/pts/7 --portOptions {}
+ *    node app.js --probePort=/dev/pts/7 --probeOptions {}
  *
  */
  
