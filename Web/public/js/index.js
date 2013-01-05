a0098d63b425b9ca92e416eedc4dee60643620c9
Adam Ulvi <adam@delaris.com>
2013-01-04 17:42:17 -0800

    Finished wiring up the front-end, now able to display the temp.

--- Web/public/js/index.js
+++ Web/public/js/index.js
@@ -7,9 +7,16 @@
 
 	var socket = io.connect('http://localhost');
 
+	socket.on('connect', function() {
+		console.log("Connecting");
+		socket.emit('getTemp');
+	});
+
 	socket.on('getTemp', function (data) {
 		console.log('Received getTemp socket event!');
 		console.log(data);
+		$("#primary-uom").html((9/5 * data.temperature + 32).toPrecision(2) + "&#8457");
+		$("#humidity").html(data.humidity + "% humidity");
 	});
 
 	$("#updateNow").live('click', function() {
