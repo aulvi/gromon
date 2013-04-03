b61a34f5950064014efcacb8937e62c8b7e9ff17
Adam Ulvi <adam@delaris.com>
2013-04-02 22:01:30 -0700

    Updated the sketch, removed a bunch of crappy logging. Cleaned up the init process.

--- Arduino/GroMon/GroMon.ino
+++ Arduino/GroMon/GroMon.ino
@@ -1,6 +1,6 @@
 /*
  * Sketch: GroMon.ino
- *   Desc: 
+ *   Desc: Provide temperature and humidity data across a Bluetooth connection.
  *
  * Required Arduino libraries:
  *   SoftwareSerial - http://arduino.cc/en/Reference/SoftwareSerial
@@ -21,10 +21,9 @@
 #define dht11_pin 5  // Temperature sensor pin
 
 SoftwareSerial blueToothSerial(RxD,TxD);
-String btSerialInput = "";
-boolean btSerialComplete = false;
-String usbSerialInput = "";
-boolean usbSerialComplete = false;
+String blueInput= "";
+boolean blueReady = false;
+boolean debug = true;
 
 //
 // Set it up!
@@ -33,107 +32,158 @@ void setup() {
 
 	pinMode(RxD, INPUT);
 	pinMode(TxD, OUTPUT);
-    pinMode(dht11_pin, OUTPUT);
-    digitalWrite(dht11_pin, HIGH);
+	pinMode(dht11_pin, OUTPUT);
+	digitalWrite(dht11_pin, HIGH);
 
-    setupBlueToothConnection();
-    sendBlueToothCommand("{status::ready}\r\n");
+	// Set up the bluetooth device.
+	setupBlueToothConnection();
 }
 
 void setupBlueToothConnection() {
-    Logln("Setting up Bluetooth connection..");
+	if (debug) {
+		Serial.println("Setting up Bluetooth connection..");
+	}
 
-    blueToothSerial.begin(38400); //Set Bluetooth Bee to default baud rate 38400
-    delay(2000);
+	blueToothSerial.begin(38400); //Set Bluetooth Bee to default baud rate 38400
+	delay(2000);
 
-    sendBlueToothCommand("\r\n+STWMOD=0\r\n");    // 0 = slave
-    sendBlueToothCommand("\r\n+STNA=ProbeMon\r\n"); 
-    sendBlueToothCommand("\r\n+STAUTO=0\r\n");    // 0 = Auto-connect disabled 
-    sendBlueToothCommand("\r\n+STOAUT=1\r\n");    // 1 = Permit Paired device to connect me
-    sendBlueToothCommand("\r\n+STPIN=0000\r\n");
-    delay(2000); // This delay is required.
+	sendConfigCmd("\r\n+STWMOD=0\r\n");    // 0 = slave
+	sendConfigCmd("\r\n+STNA=ProbeMon\r\n"); 
+	sendConfigCmd("\r\n+STAUTO=0\r\n");    // 0 = Auto-connect disabled 
+	sendConfigCmd("\r\n+STOAUT=1\r\n");    // 1 = Permit Paired device to connect me
+	sendConfigCmd("\r\n+STPIN=0000\r\n");
+	delay(2000); // This delay is required.
 
-    sendBlueToothCommand("\r\n+INQ=1\r\n");      // 1 = Enable been inquired (slave)
-    delay(2000); // This delay is required.
+	sendConfigCmd("\r\n+INQ=1\r\n");      // 1 = Enable been inquired (slave)
+	delay(2000); // This delay is required.
 
-	Logln("..done!");
+	if (debug) {
+		Serial.println("..done!");
+	}
+}
+
+// This sends commands to the Bluetooth module.
+void sendConfigCmd(char command[]) {
+	blueToothSerial.print(command);
+	checkOk();
+
+	if (debug) {
+		Serial.print("Bluetooth sending: ");
+		Serial.println(command);
+	}
 }
 
 // This sends commands to the Bluetooth module.
 void sendBlueToothCommand(char command[]) {
 	blueToothSerial.print(command);
-    Log("Bluetooth sending: ");
-    Logln(command);
+	checkOk();
+
+	if (debug) {
+		Serial.print("Bluetooth sending: ");
+		Serial.println(command);
+	}
 }
 
 //
 // After setup completes, run the main loop.
 //
 void loop() {
-	readBtSerial();	// Read bluetooth data into btSerialInput.
-	readUsbSerial();	// Read USB serial port data into usbSerialInput, this is useful for debugging.
-	commandLoop();	// Take action on the command strings.
+	readBtSerial(); // Read bluetooth data into blueInput.
+	commandLoop(); // Take action on the command strings.
 }
 
+// This function reads incoming data from the Bluetooth module
+// and adds it to a "command string" variable named blueInput.
+// We can only "eat" one character each time loop() is called, so 
+// we use a boolean variable named blueReady to tell the rest
+// of our program whether or not we have a full "command string" to
+// take action on.
+// Later, in the commandLoop() we read the "command string" and
+// take appropriate action.
 void readBtSerial() {
-	while (blueToothSerial.available()){
+	while (blueToothSerial.available()) {
 		char in = (char) blueToothSerial.read();
 
-		Log("Bluetooth: Char is: ");
-		Logln(in);
-
+		// If we hit a newline (or exlamation point) then we
+		// have a full "command string" and can take action
+		// when commandLoop() is called.
 		if (in == '!' || in == '\n'){
-			btSerialComplete = true;
-			Logln("Bluetooth: Exclam or newline found!");
+			blueReady = true;
 		} else {
-			btSerialInput += in;
+			blueInput += in;
 		}
 	}
 }
 
-void readUsbSerial() {
-	while(Serial.available()){
-		char in = (char) Serial.read();
+// Checks if the response "OK" is received
+void checkOk() {
+	char a,b;
+	while(1) {
+		if (blueToothSerial.available()) {
+			a = blueToothSerial.read();
 
-		Log("USB: Char is: ");
-		Logln(in);
+			if (debug) {
+				Serial.write(a);    // show response from bluetooth module
+			}
 
-		if (in == '!' || in == '\n'){
-			usbSerialComplete = true;
-			Logln("USB: Exclam or newline found!");
-		} else {
-			usbSerialInput += in;
+			// Wait for next character K. The available() function is 
+			// required in case K is not immediately available.
+			if ('O' == a) {
+				while (blueToothSerial.available()) {
+					b = blueToothSerial.read();
+					if (debug) {
+						Serial.write(b);        
+					}
+					break;
+				}
+				if('K' == b) {
+					break;
+				}
+			}
 		}
 	}
+
+	//Wait until all other response chars are received
+	while ((a = blueToothSerial.read()) != -1) {}
 }
 
+// 
 void commandLoop() {
-	if (btSerialComplete == true) {
-		Logln("Bluetooth serial command is: " + btSerialInput);
+	if (blueReady == true) {
+		if (debug) {
+			Serial.println("Bluetooth serial command is: " + blueInput);
+		}
 
-		handleBtInput(btSerialInput);
-		btSerialComplete = false;
-	}
-
-	if (usbSerialComplete == true) {
-		Logln("USB serial command: " + usbSerialInput);
-		handleUsbInput(usbSerialInput);
+		handleBtInput(blueInput);
+		blueReady = false;
 	}
 }
 
 void handleBtInput(String& input) {
 	if (input == "cmd::getTemp"){
-		Logln("Fetching temp!");
+		if (debug) {
+			Serial.println("Fetching temp!");
+		}
 		getTemp();
 	}
 	else if (input == "cmd::getVersion") {
-		Logln("Returning version " + VERSION);
+		if (debug) {
+			Serial.println("Returning version 0.1");
+		}
 		sendBlueToothCommand("0.1");
 	}
-
+	else if (input == "cmd::debug") {
+		debug = !debug;
+		if (debug) {
+			Serial.println("Debug enabled");
+		} else {
+			Serial.println("Debug disabled");
+		}
+	}
 	input = "";
 }
 
+// Fetch the temperature and humidity from the DHT11 sensor.
 void getTemp() {
 	byte dht11_dat[5];   
 	byte dht11_in;
@@ -165,81 +215,40 @@ void getTemp() {
 		dht11_dat[i] = read_dht11_dat();
 	}  //recieved 40 bits data. Details are described in datasheet
     
-  pinMode(dht11_pin, OUTPUT);
-  digitalWrite(dht11_pin, HIGH);
-  byte dht11_check_sum = dht11_dat[0]+dht11_dat[2];// check check_sum
-  if(dht11_dat[4]!= dht11_check_sum)
-  {
-    Serial.println("DHT11 checksum error");
-  }
+	pinMode(dht11_pin, OUTPUT);
+	digitalWrite(dht11_pin, HIGH);
+	byte dht11_check_sum = dht11_dat[0]+dht11_dat[2];// check check_sum
 
-  Serial.print("Current humidity = ");
-  Serial.print(dht11_dat[0], DEC);
-  Serial.print("%  ");
-  Serial.print("temperature = ");
-  Serial.print(dht11_dat[2], DEC);
-  Serial.println("C");
+	if (dht11_dat[4]!= dht11_check_sum) {
+		Serial.println("DHT11 checksum error");
+	}
 
-  int tempC = dht11_dat[2];
-  char buffer[40];
-  
-  sprintf(buffer, "{\"temperature\": %d, \"humidity\": %d}\n", tempC, dht11_dat[0]);
-  blueToothSerial.print(buffer);
+	Serial.print("Current humidity = ");
+	Serial.print(dht11_dat[0], DEC);
+	Serial.print("%  ");
+	Serial.print("temperature = ");
+	Serial.print(dht11_dat[2], DEC);
+	Serial.println("C");
+
+	int tempC = dht11_dat[2];
+	char buffer[40];
+
+	sprintf(buffer, "{\"temperature\": %d, \"humidity\": %d}\n", tempC, dht11_dat[0]);
+	blueToothSerial.print(buffer);
 
 }
 
 byte read_dht11_dat() {
-  byte i = 0;
-  byte result=0;
-  for(i=0; i< 8; i++)
-  {
-    while (!digitalRead(dht11_pin));
-    delayMicroseconds(30);
-    if (digitalRead(dht11_pin) != 0 )
-      bitSet(result, 7-i);
-    while (digitalRead(dht11_pin));
-  }
-  return result;
+	byte i = 0;
+	byte result=0;
+	for (i=0; i< 8; i++) {
+		while (!digitalRead(dht11_pin));
+		delayMicroseconds(30);
+		if (digitalRead(dht11_pin) != 0) {
+			bitSet(result, 7-i);
+		}
+		while (digitalRead(dht11_pin));
+	}
+	return result;
 }
 
-//
-// Logging functions
-//
-
-// Log without a line break (ie, add your own).
-void Log(String& input) {
-  if (Serial) {
-    Serial.print(input);
-  }
-}
-
-void Log(char* input) {
-  if (Serial) {
-    Serial.print(input);
-  }
-}
-
-void Log(char input) {
-  if (Serial) {
-    Serial.print(input);
-  }
-}
-
-// Log using a "println".
-void Logln(String& input) {
-  if (Serial) {
-    Serial.println(input);
-  }
-}
-
-void Logln(char* input) {
-  if (Serial) {
-    Serial.println(input);
-  }
-}
-
-void Logln(char input) {
-  if (Serial) {
-    Serial.println(input);
-  }
-}
